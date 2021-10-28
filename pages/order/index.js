18f9b80cb3539a544b5954ceeed6d0b665649682
Evatruy <244280674@qq.com>
2021-10-28 17:24:38 +0800

    订单查询-引入tabs组件

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,11 +1,28 @@
 // pages/order/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tabs:[
+      {
+        id:0,
+        value:"全部",
+        isActive:true
+      },
+      {
+        id:1,
+        value:"待付款",
+        isActive:false
+      },
+      {
+        id:2,
+        value:"待收货",
+        isActive:false
+      },
+      {
+        id:3,
+        value:"退款/退货",
+        isActive:false
+      }
+    ]
   },
 
   /**
@@ -15,32 +32,17 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  //标题点击事件 从子组件传递过来
+  handleTabsItemChange(e){
+    // 1 获取被点击的标题索引
+    const {index}=e.detail;
+    // 2 修改源数组
+    let {tabs}=this.data;
+    tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
+    // 3 赋值到data中
+    this.setData({
+      tabs
+    })
   },
 
   /**
