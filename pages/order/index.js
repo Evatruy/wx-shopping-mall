58e4e4f4e84f04c115d15fd48e46367675872b03
Evatruy <244280674@qq.com>
2021-11-03 14:20:27 +0800

    订单查询-页面动态渲染

--- pages/order/index.js
+++ pages/order/index.js
@@ -55,7 +55,7 @@ Page({
   async getOrders(type){
     const res=await request({url:"/my/orders/all",data:{type}});
     this.setData({
-      orders:res.orders
+      orders:res.orders.map(v=>({...v,create_time_cn:(new Date(v.create_time*1000).toLocaleString())}))
     });
   },
 
