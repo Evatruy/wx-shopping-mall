a27220b8059c664aca6b4c6d73a8f922d218a145
Evatruy <244280674@qq.com>
2021-11-03 17:29:57 +0800

    意见反馈-静态页面

---
+++ pages/feedback/index.js
@@ -0,0 +1,34 @@
+// pages/feedback/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabs:[
+      {
+        id:0,
+        value:"体验问题",
+        isActive:true
+      },
+      {
+        id:1,
+        value:"商品、商家投诉",
+        isActive:false
+      }
+    ]
+  },
+
+  //标题点击事件 从子组件传递过来
+  handleTabsItemChange(e){
+    // 1 获取被点击的标题索引
+    const {index}=e.detail;
+    // 2 修改源数组
+    let {tabs}=this.data;
+    tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
+    // 3 赋值到data中
+    this.setData({
+      tabs
+    })
+  },
+})
